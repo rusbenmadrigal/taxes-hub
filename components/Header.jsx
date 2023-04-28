d735fb19731ccb3c56f2d662d02ab32d0b05516f
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-04-27 22:48:26 -0600

    Header

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import Link from "next/link";
+
+const categories = [
+  { name: "Home", slug: "/" },
+  { name: "About", alug: "/about" },
+];
+
+const Header = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-ointer font-bold text-4xl text-white">
+              GraphCMS
+            </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category) => (
+            <Link key={category.alug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-import 'tailwindcss/tailwind.css'
-import '../styles/globals.scss'
+import React, { useEffect, useState } from "react";
+import { Layout } from "@/components/Layout";
+
+import "tailwindcss/tailwind.css";
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
