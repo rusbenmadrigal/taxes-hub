93bd23f29db3fb2508b0a66677c87df90f293f32
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-04-28 00:00:16 -0600

    Featured Image

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 /* eslint-disable react/jsx-key */
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "@/components";
+import { getPosts } from "../services";
 
-const post = [
-  { title: "My first post", excerpt: "This is my first post" },
-  { title: "My second post", excerpt: "This is my second post" },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -16,8 +12,8 @@ export default function Home() {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {post.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+          {posts.map((post) => (
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -29,4 +25,11 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
 }
