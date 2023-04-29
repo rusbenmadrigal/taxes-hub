61a20a67c96eecf3fa4e2dfe6d4aff17caa9aeb3
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-04-28 22:10:35 -0600

    Main Menu Update

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-
-const categories = [
-  { name: "Home", slug: "/" },
-  { name: "About", alug: "/about" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
@@ -19,7 +19,7 @@ const Header = () => {
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
-            <Link key={category.alug} href={`/category/${category.slug}`}>
+            <Link key={category.slug} href={`/category/${category.slug}`}>
               <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -8,11 +8,47 @@ const PostWidget = ({ categories, slug }) => {
 
   useEffect(() => {
     if (slug) {
-      getSimilarPosts(categories, slug).then((result) => setRelatedPosts(result));
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
     } else {
       getRecentPosts().then((result) => setRelatedPosts(result));
     }
   }, [slug]);
-  return <div>PostWidgets</div>;
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="aling-middle rounded-full"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </p>
+            <Link
+              href={`/blog/${post.slug}`}
+              key={post.title}
+              className="text-md"
+            >
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
+
 export default PostWidget;
