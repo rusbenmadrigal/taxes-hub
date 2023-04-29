4d2c200e7b5a9843ac6d6fe95dc8f1fe29c2a15d
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-04-28 18:32:02 -0600

    PostWidget Component

--- Services/index.js
+++ Services/index.js
@@ -32,6 +32,48 @@ export const getPosts = async () => {
       }
     }
   `;
-    const result = await request(graphqlAPI, query);
-    return result.postsConnection.edges;
+  const result = await request(graphqlAPI, query);
+  return result.postsConnection.edges;
+};
+
+export const getRecentPosts = async (slug) => {
+  const query = gql`
+    query GetPostsDetails() {
+      posts(orderBy: createdAt_DESC
+        last: 3
+        ){
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+    }
+  }
+   `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostsDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
 };
