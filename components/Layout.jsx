c0fe2e41a63fdaec73900c4f5bb738da21f3b16d
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-04-29 18:49:52 -0600

    Update

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Header from './Header'
+import React from 'react';
+import Header from './Header';
 
-export const Layout = ({children}) => {
-  return (
-    <>
+const Layout = ({ children }) => (
+  <>
     <Header />
     {children}
-    </>
-  )
-}
+  </>
+);
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { Layout } from "@/components/Layout";
+import React from 'react';
 
-import "tailwindcss/tailwind.css";
-import "../styles/globals.scss";
+import '../styles/globals.scss';
+import { Layout } from '../components';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
 }
+
+export default MyApp;
