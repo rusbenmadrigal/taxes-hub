8dd973de3b5d9b1f0b61e37195c2003c23e68d4f
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-05-03 21:09:50 -0600

    Update

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -55,7 +55,7 @@ const PostCard = ({ post }) => {
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
           <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
-            Read More
+            Read Article
           </span>
         </Link>
       </div>
