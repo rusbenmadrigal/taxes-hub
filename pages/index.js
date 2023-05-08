f5ba29afb7e0f82230529d3899aa6824054dca76
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-05-07 18:30:12 -0600

    Favicon

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ posts }) {
     <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>We help you understand and control your personal finances</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.png" />
       </Head>
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
