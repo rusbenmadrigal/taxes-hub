e411a3298660b503b2a796017f546f8cfa7d45b9
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-05-12 14:08:26 -0600

    Update

--- next.config.js
+++ next.config.js
@@ -4,24 +4,15 @@
 
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
-  // All the other config options you may have...
   async redirects() {
-      return [
-          {
-              // What the user typed in the browser
-              source: '/post/ability-to-pay-taxation',
-              // Where the user will be redirected to
-              destination: '/posts/ability-to-pay-taxation',
-              // If the destination is a permanent redirect (308)
-              permanent: true
-          }
-      ];
-  }
+    return [
+      {
+        source: "/post/ability-to-pay-taxation",
+        destination: "/posts/ability-to-pay-taxation",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
-
-
